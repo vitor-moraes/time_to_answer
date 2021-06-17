ac9d3889d540a159cb37209cc26e79ceb0d68d02
vitor-moraes <vitormoraes3@yahoo.com.br>
2021-06-16 22:35:51 -0300

    feat: welcome index as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'inicio', to: 'welcome#index'
+  
+  root to: 'welcome#index'
 end
