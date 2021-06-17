bd2e0403bd4645785f4c165291c5d27d784274ab
vitor-moraes <vitormoraes3@yahoo.com.br>
2021-06-16 23:02:16 -0300

    feat: rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'inicio', to: 'welcome#index'
-  
+
   root to: 'welcome#index'
 end
